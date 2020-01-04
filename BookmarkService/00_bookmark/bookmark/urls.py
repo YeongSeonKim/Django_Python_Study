72b2a7254bd1c93956537bb67f2afd875f2c14f6
YeongSeonKim <son960311@naver.com>
2020-01-04 18:24:17 +0900

    200104 | Bookmark_숙제 VSCode 이용해서 해보기

--- BookmarkService/00_bookmark/bookmark/urls.py
+++ BookmarkService/00_bookmark/bookmark/urls.py
@@ -6,6 +6,6 @@ app_name = 'bookmark'
 urlpatterns = [
     path('', views.index, name='index'),
     # re_path(r'^$', views.index, name='index'),
-    path('bookmark/<int:bookmark_pk>/', views.detail, name='detail'),
+    path('<int:bookmark_pk>/', views.detail, name='detail'),
     # re_path(r'^bookmark/(?P<pk>\d+)/$', views.detail, name='detail'),
 ]
